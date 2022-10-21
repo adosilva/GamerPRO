2e0e80dde687bd6213ba88fafef72cc01812115f
adosilva <abimael.silva@sp.senai.br>
2022-10-20 22:06:37 -0300

    PRJ GamerPRO Rev.15

---
+++ js/map.js
@@ -0,0 +1,17 @@
+jQuery(function ($) {
+    function init_map1() {
+        var myLocation = new google.maps.LatLng(-23.4873588,-47.4393022,15);
+        var mapOption = {
+            center: myLocation,
+            zoom: 16
+        };
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            title: "Localização"
+        });
+        var map = new google.maps.Map(document.getElementById("map1"),
+            mapOptions);
+        marker.setMap(map);
+    }
+    init_map1();
+});
